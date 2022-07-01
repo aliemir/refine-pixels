2bb4793c186e0e51a2ab844eca0adc3b5fecc820
Ali Emir Sen <senaliemir@gmail.com>
2022-07-01 14:36:17 +0300

    initialize refine

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,42 @@
 import { Refine } from "@pankod/refine-core";
-import {
-  notificationProvider,
-  ReadyPage,
-  ErrorComponent,
-  LoginPage,
-} from "@pankod/refine-antd";
+import { ReadyPage, ErrorComponent } from "@pankod/refine-antd";
 import "@pankod/refine-antd/dist/styles.min.css";
 import routerProvider from "@pankod/refine-react-router-v6";
-import { dataProvider } from "@pankod/refine-supabase";
+import { dataProvider, liveProvider } from "@pankod/refine-supabase";
 import { supabaseClient } from "utility";
 import {
   Title,
   Header,
-  Sider,
   Footer,
   Layout,
   OffLayoutArea,
 } from "components/layout";
-import authProvider from "./authProvider";
+
+import "styles/index.css";
+
+import { CanvasList } from "pages/canvases/list";
+import { CanvasCreate } from "pages/canvases/create";
+import { CanvasShow } from "pages/canvases/show";
 
 function App() {
   return (
     <Refine
-      notificationProvider={notificationProvider}
       ReadyPage={ReadyPage}
       catchAll={<ErrorComponent />}
       routerProvider={routerProvider}
+      resources={[
+        {
+          name: "canvases",
+          create: CanvasCreate,
+          list: CanvasList,
+          show: CanvasShow,
+        },
+      ]}
       dataProvider={dataProvider(supabaseClient)}
-      authProvider={authProvider}
-      LoginPage={LoginPage}
+      liveProvider={liveProvider(supabaseClient)}
       Title={Title}
       Header={Header}
-      Sider={Sider}
+      Sider={() => null}
       Footer={Footer}
       Layout={Layout}
       OffLayoutArea={OffLayoutArea}
