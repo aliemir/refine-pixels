2f4b61c0c14f4294ab1fc2ad938bfea30ed8d346
Ali Emir Sen <senaliemir@gmail.com>
2022-07-01 10:44:17 +0300

    initial commit with superplate

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import { Refine } from "@pankod/refine-core";
+import {
+  notificationProvider,
+  ReadyPage,
+  ErrorComponent,
+  LoginPage,
+} from "@pankod/refine-antd";
+import "@pankod/refine-antd/dist/styles.min.css";
+import routerProvider from "@pankod/refine-react-router-v6";
+import { dataProvider } from "@pankod/refine-supabase";
+import { supabaseClient } from "utility";
+import {
+  Title,
+  Header,
+  Sider,
+  Footer,
+  Layout,
+  OffLayoutArea,
+} from "components/layout";
+import authProvider from "./authProvider";
+
+function App() {
+  return (
+    <Refine
+      notificationProvider={notificationProvider}
+      ReadyPage={ReadyPage}
+      catchAll={<ErrorComponent />}
+      routerProvider={routerProvider}
+      dataProvider={dataProvider(supabaseClient)}
+      authProvider={authProvider}
+      LoginPage={LoginPage}
+      Title={Title}
+      Header={Header}
+      Sider={Sider}
+      Footer={Footer}
+      Layout={Layout}
+      OffLayoutArea={OffLayoutArea}
+    />
+  );
+}
+
+export default App;
