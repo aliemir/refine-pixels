1c34438349fc0011c3b8e5a06c17edda0288821c
Ali Emir Sen <senaliemir@gmail.com>
2022-07-01 14:36:07 +0300

    add supabase client credentials

--- src/utility/supabaseClient.ts
+++ src/utility/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@pankod/refine-supabase";
 
-const SUPABASE_URL = "https://iwdfzvfqbtokqetmbmbp.supabase.co";
+const SUPABASE_URL = "https://ujcwhefhzncpphzxolnt.supabase.co";
 const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMDU2NzAxMCwiZXhwIjoxOTQ2MTQzMDEwfQ._gr6kXGkQBi9BM9dx5vKaNKYj_DJN1xlkarprGpM_fU";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVqY3doZWZoem5jcHBoenhvbG50Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTY2NTk3MDMsImV4cCI6MTk3MjIzNTcwM30.Ji4_ioiiJaGsziXlcKLwkSForDOTFJMRmvZZmLyo8Zw";
 
 export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
